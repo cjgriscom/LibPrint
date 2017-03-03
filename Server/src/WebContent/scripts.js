b5a74e40c956eafc9556befaece8c19ddab6ac50
CJ Griscom <cjgriscom@comcast.net>
2017-03-03 11:30:27 -0500

    Move SGUI to WebContent, get JSON working

--- Server/src/WebContent/scripts.js
+++ Server/src/WebContent/scripts.js
@@ -28,18 +28,20 @@ function callOtherDomain() {
 
 // Builds the HTML Table out of myList.
 function buildHtmlTable(selector) {
-	//myList = $.getJSON("http://chandler.io/LibPrint/RequestHandler?request=listQueue");
-	var columns = addAllColumnHeaders(myList, selector);
+	$.getJSON(url, function(result){
+		myList = result.queue;
+        var columns = addAllColumnHeaders(myList, selector);
 
-	for (var i = 0; i < myList.length; i++) {
-		var row$ = $('<tr/>');
-		for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-			var cellValue = myList[i][columns[colIndex]];
-			if (cellValue == null) cellValue = "";
-			row$.append($('<td/>').html(cellValue));
-		}
-		$(selector).append(row$);
-	}
+    	for (var i = 0; i < myList.length; i++) {
+    		var row$ = $('<tr/>');
+    		for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+    			var cellValue = myList[i][columns[colIndex]];
+    			if (cellValue == null) cellValue = "";
+    			row$.append($('<td/>').html(cellValue));
+    		}
+    		$(selector).append(row$);
+    	}
+    });
 }
 
 
