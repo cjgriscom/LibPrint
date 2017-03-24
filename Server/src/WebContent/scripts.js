0b509c714f371d4c14299328357ccb20e23b0af7
CJ Griscom <cjgriscom@comcast.net>
2017-03-23 22:12:29 -0400

    Move new HTML into WebContent

--- Server/src/WebContent/scripts.js
+++ Server/src/WebContent/scripts.js
@@ -1,6 +1,5 @@
 var req = new XMLHttpRequest();
 var url = "http://chandler.io/LibPrint/RequestHandler?request=listQueue";
-var myList;
 
 function callOtherDomain() {
 	if ('withCredentials' in req) {
@@ -19,36 +18,102 @@ function callOtherDomain() {
 }
 }
 //http://stackoverflow.com/questions/5180382/convert-json-data-to-a-html-table
-//var myList = [
-//  { "name": "abc", "age": 50 },
-//  { "age": "25", "hobby": "swimming" },
-//  { "name": "xyz", "hobby": "programming" }
+//var myList = [//...
+  //{ "name": "abc", "age": 50 },
+  //{ "age": "25", "hobby": "swimming" },
+ // { "name": "xyz", "hobby": "programming" }
 //];
 
+//$(document).ready(function() {
+ //           $("#refresh").click(function(event){
+  //             $('#temp').load('http://chandler.io/LibPrint/RequestHandler?request=listQueue');
+   //         });
+    //     });
+function clearTable(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+$(document).ready(function() {
+	$("#refresh").click(function(event){
+		
+		$.getJSON('http://chandler.io/LibPrint/RequestHandler?request=listQueue', function(jd) {
+			$('#queue').empty();
+			buildHtmlTable(queue, jd.queue);
+		});
+			
+		$.getJSON('http://chandler.io/LibPrint/RequestHandler?request=listHistory', function(jd) {
+			$('#hist').empty();
+			buildHtmlTable(hist, jd.history);
+		});
+			
+	});
+	
+	$(".bgre").click(function(event){
+		alert("here");
+		alert($(this).ID.replace("prb",""));
+		acceptPrint($(this).ID.replace("prb",""),true);
+	});
+	
+	$(".bred").click(function(event){
+		alert("here");
+		alert($(this).ID.replace("cab",""));
+		acceptPrint($(this).ID.replace("cab",""),false);
+	});
+	
+	$("#highlight").click(function(event){
+    var frm = $('.htaccess');
+    var dat = JSON.stringify(frm.serializeArray());
 
-// Builds the HTML Table out of myList.
-function buildHtmlTable(selector) {
-	$.getJSON(url, function(result){
-		myList = result.queue;
-        var columns = addAllColumnHeaders(myList, selector);
+    alert("I am about to POST this:\n\n" + dat);
 
-    	for (var i = 0; i < myList.length; i++) {
-    		var row$ = $('<tr/>');
-    		for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-    			var cellValue = myList[i][columns[colIndex]];
-    			if (cellValue == null) cellValue = "";
-    			row$.append($('<td/>').html(cellValue));
-    		}
-    		$(selector).append(row$);
-    	}
-    });
+	$.post(
+		 frm.attr("action"),
+         dat,
+         function(data) {
+           alert("Response: " + data);
+         }
+       );
+     });
+});
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+
+function acceptPrint(id,accept) {
+     $.post('http://chandler.io/LibPrint/RequestHandler', { request: (accept ? "acceptPrint" : "rejectPrint"), ID : id},
+         function(returnedData){
+             callOtherDomain(); // Refresh tablee
+			 if(returnData.toString().contains("error")){
+				 alert("Error");
+             }
+         }).fail(function(){
+             alert("This operation failed to complete.");
+         });
+}
+
+function buildHtmlTable(selector, myList) {
+	var columns = addAllColumnHeaders(selector,myList);
+	var rownum=0;
+	for (var i = 0; i < myList.length; i++) {
+		var row$ = $('<tr/>');
+		for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+			var cellValue = myList[i][columns[colIndex]];
+			if (cellValue == null) cellValue = "";
+			row$.append($('<td/>').html(cellValue));
+		}
+		row$.append($('<td/>').html("<a\"https://www.google.com/cloudprint/learn/\" id=\"prb"+rownum+"\" class=\"button bgre\"> Print </a>"));
+		row$.append($('<td/>').html("<a \" id=\"cab"+rownum+"\" class=\"button bred\"> Cancel </a>"));
+		rownum++;
+		$(selector).append(row$);
+	}
 }
 
 
 // Adds a header row to the table and returns the set of columns.
 // Need to do union of keys from all records as some records may not contain
 // all records.
-function addAllColumnHeaders(myList, selector) {
+function addAllColumnHeaders(selector,myList) {
 	var columnSet = [];
 	var headerTr$ = $('<tr/>');
 
@@ -61,6 +126,9 @@ function addAllColumnHeaders(myList, selector) {
 			}
 		}
 	}
+	headerTr$.append($('<th/>').html("Print"));
+	headerTr$.append($('<th/>').html("Cancel"));
+	
 	$(selector).append(headerTr$);
 	return columnSet;
 }
